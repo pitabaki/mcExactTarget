4d12e104c7571e25e03ebb3623e6885328a52347
Peter Berki <pberki1986@gmail.com>
2015-11-08 16:45:59 -0800

    Annotations available.

--- js/func.js
+++ js/func.js
@@ -18,30 +18,41 @@ var func = {
 	},// END SINGLEOPER
 	multiOper: function(chars,blank,ref,sngCl,dblCl){
 		'use strict';
+
+		//Function modifies passed variables
 		function rest(word, res){
 			res += word + "</span>";
 			return res;
 		}
-		//Modify passed variables
+
+		//modify symantic class (class for single elements)
+		sngCl += chars[0] + "</span>";
+
+		//variables for word lengths. Add to i in for loop.
 		var sixChars = chars[2].length - 1,
 			fiveChars = chars[3].length - 1,
 			fourChars = chars[4].length - 1;
-		sngCl += chars[0] + "</span>";
+
 		//Loop through returned HTML and apply <span> class
 		for(var i = 0; i < ref.length; i++){
+
+			//Variables used to check for matched characters at index number indicated below.
 			var g = i + 1,
 				h = i + 2,
 				j = i + 3,
-				m = i + 4;
+				m = i + 5;
 			if((ref[i] === chars[1][0]) && (ref[g] === chars[1][1])){
 				blank.push(rest(chars[1],dblCl));
 				i = i + 1;
-			}else if((ref[i] === chars[2][0]) && (ref[g] === chars[2][1]) && (ref[h] === chars[2][2]) && (ref[j] === chars[2][3]) && (ref[m] === chars[2][4])){
+			}else if((ref[i] === chars[2][0]) && (ref[m] === chars[2][5])){
 				blank.push(rest(chars[2],dblCl));
 				i = i + sixChars;
 			}else if((ref[i] === chars[3][0]) && (ref[h] === chars[3][2])){
 				blank.push(rest(chars[3],dblCl));
 				i = i + fiveChars;
+			}else if((ref[i] === chars[4][0]) && (ref[h] === chars[4][2])){
+				blank.push(rest(chars[4],dblCl));
+				i = i + fourChars;
 			}else if(ref[i] === chars[0][0]){
 				blank.push(sngCl);
 			}else{
